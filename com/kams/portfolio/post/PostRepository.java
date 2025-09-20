19466529265186e5e9a50fe8a2e71dccdafed475
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-09-19 23:52:25 -0400

    Add files via upload

---
+++ com/kams/portfolio/post/PostRepository.java
@@ -0,0 +1,8 @@
+package com.kams.portfolio.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByType(String type);
+}
