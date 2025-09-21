9f1ff5473135bbc4c1fe4cfa59165043c504f254
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-09-20 21:44:55 -0400

    Update PostRepository.java

--- com/kams/portfolio/post/PostRepository.java
+++ com/kams/portfolio/post/PostRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findByType(String type);
+
+    List<Post> findByTypeAndPublished(String type, boolean published);
+
+    List<Post> findByPublishedTrue();
 }
